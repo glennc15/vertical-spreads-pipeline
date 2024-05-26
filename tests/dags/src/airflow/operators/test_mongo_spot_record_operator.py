9b862e61af14712b65a053e9cb3a14219e7727c1
Glenn <gcrosby15@gmail.com>
2024-05-25 19:29:15 -0500

    need to build a test db for testing

--- tests/dags/src/airflow/operators/test_mongo_spot_record_operator.py
+++ tests/dags/src/airflow/operators/test_mongo_spot_record_operator.py
@@ -1,6 +1,19 @@
+from airflow.models.connection import Connection
+
+from pytest_mock import mocker
 from dags.src.airflow.operators.mongo_spot_record_operator import MongoSpotRecordOperator
 
-def test_mongo_spot_record_operator():
+def test_mongo_spot_record_operator(mocker):
+    mocker.patch.object(
+        MongoSpotRecordOperator,
+        "get_connection",
+        return_value=Connection(
+            conn_id="test_mongo",
+            login="airflow",
+            password="airflow"
+        )
+    )
+
     task = MongoSpotRecordOperator(
         task_id="test",
         conn_id="test_mongo",
