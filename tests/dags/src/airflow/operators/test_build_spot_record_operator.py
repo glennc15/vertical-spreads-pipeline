28c7538346901973a962ea677f1ab886dfefbed7
Glenn <gcrosby15@gmail.com>
2024-05-30 14:08:27 -0500

    further refactoring of test_build_spot_record_operator.py. Moved all the code to run the operator into a function. Now each test runs this function and tests if the postgres database was updated correctly. All tests passing

--- tests/dags/src/airflow/operators/test_build_spot_record_operator.py
+++ tests/dags/src/airflow/operators/test_build_spot_record_operator.py
@@ -9,12 +9,11 @@ from dags.src.airflow.operators.build_spot_record_operator import BuildSpotRecor
 sql_files_path = '/Users/glenn/Documents/DataEngineering/vertical-spreads-pipeline/dags/sql'
 
 
-def test_build_spot_record_empty_postgres_db_record_timestamp(mocked_mongo_hook, mocked_postgres_hook):
-
+def run_build_spot_record_operator(mocked_mongo_hook, mocked_postgres_hook, postgres_init_filename):
     # initialize postgres with no spot records:
     pg_hook = PostgresHook()
 
-    init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_no_records.sql")
+    init_file = os.path.join(os.path.dirname(__file__), "sql", postgres_init_filename)
 
     with open(init_file, "r") as f:
         sql = f.read()
@@ -37,6 +36,44 @@ def test_build_spot_record_empty_postgres_db_record_timestamp(mocked_mongo_hook,
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
     test_records = cursor.fetchall()
+
+    return test_records
+
+
+def test_build_spot_record_empty_postgres_db_record_timestamp(mocked_mongo_hook, mocked_postgres_hook):
+
+    # # initialize postgres with no spot records:
+    # pg_hook = PostgresHook()
+
+    # init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_no_records.sql")
+
+    # with open(init_file, "r") as f:
+    #     sql = f.read()
+
+    # pg_hook.run(sql)
+
+    # # run the task:
+    # task = BuildSpotRecordOperator(
+    #     task_id="test",
+    #     postgres_conn_id="test_postgres",
+    #     mongo_conn_id="test_mongo",
+    #     sql_path=os.path.join(sql_files_path, "get_latest_timestamp.sql")
+    # )
+
+    # task.execute(context=())
+
+    # # test postgres was updated properly:
+    # # get the latest record from postgres:
+    # conn = pg_hook.get_conn()
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
+    # test_records = cursor.fetchall()
+
+    test_records = run_build_spot_record_operator(
+        mocked_mongo_hook=mocked_mongo_hook,
+        mocked_postgres_hook=mocked_postgres_hook,
+        postgres_init_filename="postgres_no_records.sql"
+    )
 
     # test expiration:
     expected_timestamp = datetime.datetime.fromisoformat("2020-09-10T15:45:00.000+00:00")
@@ -46,32 +83,39 @@ def test_build_spot_record_empty_postgres_db_record_timestamp(mocked_mongo_hook,
 
 def test_build_spot_record_empty_postgres_db_spot(mocked_mongo_hook, mocked_postgres_hook):
 
-    # initialize postgres with no spot records:
-    pg_hook = PostgresHook()
+    # # initialize postgres with no spot records:
+    # pg_hook = PostgresHook()
 
-    init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_no_records.sql")
+    # init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_no_records.sql")
 
-    with open(init_file, "r") as f:
-        sql = f.read()
+    # with open(init_file, "r") as f:
+    #     sql = f.read()
 
-    pg_hook.run(sql)
+    # pg_hook.run(sql)
 
-    # run the task:
-    task = BuildSpotRecordOperator(
-        task_id="test",
-        postgres_conn_id="test_postgres",
-        mongo_conn_id="test_mongo",
-        sql_path=os.path.join(sql_files_path, "get_latest_timestamp.sql")
+    # # run the task:
+    # task = BuildSpotRecordOperator(
+    #     task_id="test",
+    #     postgres_conn_id="test_postgres",
+    #     mongo_conn_id="test_mongo",
+    #     sql_path=os.path.join(sql_files_path, "get_latest_timestamp.sql")
+    # )
+
+    # task.execute(context=())
+
+    # # test postgres was updated properly:
+    # # get the latest record from postgres:
+    # conn = pg_hook.get_conn()
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
+    # test_records = cursor.fetchall()
+
+    test_records = run_build_spot_record_operator(
+        mocked_mongo_hook=mocked_mongo_hook,
+        mocked_postgres_hook=mocked_postgres_hook,
+        postgres_init_filename="postgres_no_records.sql"
     )
 
-    task.execute(context=())
-
-    # test postgres was updated properly:
-    # get the latest record from postgres:
-    conn = pg_hook.get_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
-    test_records = cursor.fetchall()
 
     # test spot:
     expected_spot = 340.77
@@ -79,34 +123,43 @@ def test_build_spot_record_empty_postgres_db_spot(mocked_mongo_hook, mocked_post
     assert test_spot == expected_spot
 
 
+
 def test_build_spot_record_one_previous_record_timestamp(mocked_mongo_hook, mocked_postgres_hook):
 
-    # initialize postgres with one spot records:
-    pg_hook = PostgresHook()
+    # # initialize postgres with one spot records:
+    # pg_hook = PostgresHook()
 
-    init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_one_record.sql")
+    # init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_one_record.sql")
 
-    with open(init_file, "r") as f:
-        sql = f.read()
+    # with open(init_file, "r") as f:
+    #     sql = f.read()
 
-    pg_hook.run(sql)
+    # pg_hook.run(sql)
 
-    postgres_sql_query = os.path.join(sql_files_path, "get_latest_timestamp.sql")
+    # postgres_sql_query = os.path.join(sql_files_path, "get_latest_timestamp.sql")
 
-    task = BuildSpotRecordOperator(
-        task_id="test",
-        postgres_conn_id="test_postgres",
-        mongo_conn_id="test_mongo",
-        sql_path=postgres_sql_query
+    # task = BuildSpotRecordOperator(
+    #     task_id="test",
+    #     postgres_conn_id="test_postgres",
+    #     mongo_conn_id="test_mongo",
+    #     sql_path=postgres_sql_query
+    # )
+
+    # task.execute(context=())
+
+    # # get the latest record from postgres:
+    # conn = pg_hook.get_conn()
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
+    # test_records = cursor.fetchall()
+
+    # postgres_one_record.sql
+
+    test_records = run_build_spot_record_operator(
+        mocked_mongo_hook=mocked_mongo_hook,
+        mocked_postgres_hook=mocked_postgres_hook,
+        postgres_init_filename="postgres_one_record.sql"
     )
-
-    task.execute(context=())
-
-    # get the latest record from postgres:
-    conn = pg_hook.get_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
-    test_records = cursor.fetchall()
 
     # test the timestamp:
     expected_timestamp = datetime.datetime.fromisoformat("2020-12-04T16:50:00.000+00:00")
@@ -117,33 +170,42 @@ def test_build_spot_record_one_previous_record_timestamp(mocked_mongo_hook, mock
 
 def test_build_spot_record_one_previous_record_spot(mocked_mongo_hook, mocked_postgres_hook):
 
-    # initialize postgres with one spot records:
-    pg_hook = PostgresHook()
+    # # initialize postgres with one spot records:
+    # pg_hook = PostgresHook()
 
-    # init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_no_records.sql")
-    init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_one_record.sql")
+    # # init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_no_records.sql")
+    # init_file = os.path.join(os.path.dirname(__file__), "sql", "postgres_one_record.sql")
 
-    with open(init_file, "r") as f:
-        sql = f.read()
+    # with open(init_file, "r") as f:
+    #     sql = f.read()
 
-    pg_hook.run(sql)
+    # pg_hook.run(sql)
 
-    postgres_sql_query = os.path.join(sql_files_path, "get_latest_timestamp.sql")
+    # postgres_sql_query = os.path.join(sql_files_path, "get_latest_timestamp.sql")
 
-    task = BuildSpotRecordOperator(
-        task_id="test",
-        postgres_conn_id="test_postgres",
-        mongo_conn_id="test_mongo",
-        sql_path=postgres_sql_query
+    # task = BuildSpotRecordOperator(
+    #     task_id="test",
+    #     postgres_conn_id="test_postgres",
+    #     mongo_conn_id="test_mongo",
+    #     sql_path=postgres_sql_query
+    # )
+
+    # task.execute(context=())
+
+    # # get the latest record from postgres:
+    # conn = pg_hook.get_conn()
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
+    # test_records = cursor.fetchall()
+
+    # postgres_one_record.sql
+
+    test_records = run_build_spot_record_operator(
+        mocked_mongo_hook=mocked_mongo_hook,
+        mocked_postgres_hook=mocked_postgres_hook,
+        postgres_init_filename="postgres_one_record.sql"
     )
 
-    task.execute(context=())
-
-    # get the latest record from postgres:
-    conn = pg_hook.get_conn()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM spots ORDER BY spot_timestamp DESC LIMIT 1;")
-    test_records = cursor.fetchall()
 
     # test the spot:
     expected_spot = 369.07
